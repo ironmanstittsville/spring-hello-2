f3d185477d21aa132414cf44d3ec2ef64ead36a6
Richard Houle <richard.houle@gmail.com>
2026-03-15 21:47:41 -0400

    initial commit

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "<h1>Success!</h1><p>Spring Boot is running in WSL2 using standard APT tools.</p>";
+    }
+}
